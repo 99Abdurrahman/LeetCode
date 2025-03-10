52364c4b12a3f61ce1f7568a590bf7f8566cd99a
Abdurrahman <94189718+99Abdurrahman@users.noreply.github.com>
2025-03-11 01:07:59 +0300

    Time: 1565 ms (67.16%) | Memory: 0B (100.00%) - LeetSync

--- MySql/577-employee-bonus/employee-bonus.sql
+++ MySql/577-employee-bonus/employee-bonus.sql
@@ -1,7 +1,7 @@
 
 
-SELECT name,b.bonus AS bonus 
+SELECT name,bonus 
 FROM Employee e
 LEFT JOIN Bonus b 
 ON e.empId = b.empId
-WHERE b.bonus < 1000 OR b.bonus IS NULL;
+WHERE bonus < 1000 OR bonus IS NULL;
