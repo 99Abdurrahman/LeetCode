eb4172c3217ae09cf5099add9813dca897b7685a
Abdurrahman <94189718+99Abdurrahman@users.noreply.github.com>
2025-03-16 10:07:00 +0300

    Time: 821 ms (62.99%) | Memory: 0B (100.00%) - LeetSync

--- MySql/584-find-customer-referee/find-customer-referee.sql
+++ MySql/584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
-SELECT C.name FROM Customer C WHERE C.referee_id!=2 OR C.referee_id IS NULL; 
+SELECT name FROM Customer C WHERE C.referee_id!=2 OR C.referee_id IS NULL; 
 
